3e6b028b88fb0aedcd5a223c0b46f5d875121953
kiev DevOps <kievdevops@gmail.com>
2024-07-23 14:54:18 -0300

    string_methods
    A few methods to check on strings

---
+++ string_methods.py
@@ -0,0 +1,21 @@
+name = "Kiev"
+
+print(len(name))  # prints the length of the string
+
+print(name.find("e"))  # prints where the specified character is
+
+print(name.capitalize())  # Capitalizes the first letter of the string
+
+print(name.upper())  # Capitalizes the entire string
+
+print(name.lower())  # Lowers the entire string
+
+print(name.isdigit())  # Checks is string is a digit
+
+print(name.isalpha())  # Checks if string has alphabetical characters
+
+print(name.count("v"))  # Count how many of the specified characters are in the string
+
+print(name.replace("e", "y"))  # Replaces characters in the string
+
+print(name*5)  # Multiplies the string by the specified amount of times
