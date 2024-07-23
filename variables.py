9a690b0987d8484bbe9b4352384b9114e38a6118
kiev DevOps <kievdevops@gmail.com>
2024-07-23 14:41:37 -0300

    variables_study
    Basic variables study

---
+++ variables.py
@@ -0,0 +1,20 @@
+# variable = container for a value
+
+first_name = "Kiev"
+last_name = "DevOps"
+full_name = first_name + " " + last_name
+
+print("Hello "+full_name)
+# string / str
+
+age = 23
+print("Your age is: "+str(age))
+# integer / int
+
+height = 1.73
+print("Your height is: "+str(height)+"m")
+# floating / float
+
+human = True
+print("Are you human? : "+str(human))
+# boolean / bool
