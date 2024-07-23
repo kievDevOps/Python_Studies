2f3c6d165313dafc3038e4a32ea1380403c3ff83
kiev DevOps <kievdevops@gmail.com>
2024-07-23 14:58:48 -0300

    type_casting
    Converting data types

---
+++ .vscode/type_casting.py
@@ -0,0 +1,13 @@
+# type casting = convert the data type to another data type
+
+x = 1  # int
+y = 2.0  # float
+z = "3"  # str
+
+x = (float(x))
+y = (int(y))
+z = (float(z))
+
+print(x)
+print(y)
+print(z)
