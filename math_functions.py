ced04c4fa16cd42728e909d6ffc01b44c523ac0f
kiev DevOps <kievdevops@gmail.com>
2024-07-23 15:08:27 -0300

    math_functions
    Studies regarding basic math functions.

---
+++ math_functions.py
@@ -0,0 +1,15 @@
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi))  # Round
+print(math.ceil(pi))  # Round up
+print(math.floor(pi))  # Round down
+print(abs(pi))  # Displays how far the number is away from zero
+print(pow(pi, 2))  # Powers the value by the specified amount
+print(math.sqrt(pi))  # Displays the squared root
+print(max(x, y, z))  # Displays the variable with the largest value
+print(min(x, y, z))  # Displays the variable with the minimum value
