f6d706bb3a2de9c8b45992bc60577d0d0ce8d83a
kiev DevOps <kievdevops@gmail.com>
2024-07-25 17:28:00 -0300

    modules
    importing modules

---
+++ modules.py
@@ -0,0 +1,9 @@
+# module = a file containing python code. May contain functions, classes, etc.
+# used with modual programming, which is to separate a program into parts
+
+import modules2 as msg
+
+msg.hello()
+msg.bye()
+
+#help("modules")  # browse through pre-written python modules
