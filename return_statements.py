9f7031bed184ac1a48fbe273fb6ccac15e711ceb
kiev DevOps <kievdevops@gmail.com>
2024-07-25 10:42:24 -0300

    return_statements
    return statements with math

---
+++ return_statements.py
@@ -0,0 +1,8 @@
+# return statement = functions send python values/ objects back to the caller
+# these values/objects are known as the function's return value
+
+def multiply(number1,number2):
+    return number1 * number2
+
+x = multiply (6,8)
+print(x)
